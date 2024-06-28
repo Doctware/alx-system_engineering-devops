b759541629a4de35004b66bb213efe4d75b2a851
Doctware <yusclever9@gmail.com>
2024-06-28 23:28:25 +0100

    creating file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
 file { '/tmp/school':
+  ensure  => 'present',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love P.uppet'
+  content => 'I love Puppet'
 }
