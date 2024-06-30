524742647ff0d673a0d4c2c76ab9403851be9c64
Doctware <yusclever9@gmail.com>
2024-07-01 00:44:15 +0100

    I love puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+file { '/home/doctware/.ssh/config':
+  ensure  => file,
+  owner   => 'doctware',
+  group   => 'doctware',
+  mode    => '0600',
+  content => @(EOF)
+Host 52.91.157.23
+    User ubuntu
+    IdentityFile /home/doctware/.ssh/config
+    PasswordAuthentication no
+| EOF
+}
