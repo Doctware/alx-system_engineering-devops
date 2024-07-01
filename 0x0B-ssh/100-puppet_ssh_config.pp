8ad2887c2dbf42c12414ad3210895f1cd1618b9a
Doctware <yusclever9@gmail.com>
2024-07-01 01:01:57 +0100

    I love puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,26 @@
 # set up clint cofig
+include stdlib
+
+file { '/home/doctware/.ssh':
+  ensure => directory,
+  owner  => 'doctware',
+  group  => 'doctware',
+  mode   => '0700',
+}
+
 file { '/home/doctware/.ssh/config':
-  ensure  => file,
-  owner   => 'doctware',
-  group   => 'doctware',
-  mode    => '0600',
-  content => @(EOF)
-Host 52.91.157.23
-    User ubuntu
-    IdentityFile ~/.ssh/config
-    PasswordAuthentication no
-| EOF
+  ensure => file,
+  owner  => 'doctware',
+  group  => 'doctware',
+  mode   => '0600',
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/home/doctware/.ssh/config',
+  line    => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path    => '/home/doctware/.ssh/config',
+  line    => '    IdentityFile ~/.ssh/school',
 }
