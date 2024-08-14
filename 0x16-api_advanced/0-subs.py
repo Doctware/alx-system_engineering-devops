b372edbd69f0d2ba4f55514966601070931e4ba5
Doctware <yusclever9@gmail.com>
2024-08-14 01:30:50 +0100

    Get number of subscriber

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,11 @@ def number_of_subscribers(subreddit):
     """ this function returns number of subscriber base give args """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "test"}
-    
-    try:
-        resp = requests.get(url, headers=headers, allow_redirects=False)
-        if resp.status_code == 200:
-            data = resp.json()
-            return data.get("data", {}).get("subscribers", 0)
-        else:
-            return 0
-    except requests.RequestException:
+
+    resp = requests.get(url, headers=headers, allow_redirects=False)
+
+    if resp.status_code == 200:
+        data = resp.json()
+        return data.get("data", {}).get("subscribers", 0)
+    else:
         return 0
