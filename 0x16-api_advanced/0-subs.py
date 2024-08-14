b42cc23325aaec6d2d655f2f7ae6aaaf2ac139ae
Doctware <yusclever9@gmail.com>
2024-08-14 01:06:37 +0100

    Get number of subscriber

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" this module fetches api from suraddit """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ this function returns number of subscriber base give args """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "subreddit-subscriber-counter"}
+
+    resp = requests.get(url, headers=headers, allow_redirects=False)
+    if resp.status_code == 200:
+        data = resp.json()
+        return data.get("data", {}).get("subscribers", 0)
+    else:
+        return 0
